72570c4062d9df3025a3b67437812fb42a89a815
Sunnyvie-ctrl <minh-nhat.bui@epita.fr>
2026-03-09 19:27:06 +0100

    feat(docker): added docker workflow and docker file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const userRoutes = require('./routes/user.routes');
+const userRoutes = require('./routes/user.route');
 app.use('/users', userRoutes);
 
 app.use(express.json());
